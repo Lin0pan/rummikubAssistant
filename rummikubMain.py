825af2135690e3d04783e6f050e2a36c23ad8135
Janek <janek.speit@gmail.com>
2016-08-30 18:23:40 +0200

    fixes

--- rummikubMain.py
+++ rummikubMain.py
@@ -1,7 +1,7 @@
 
 numberSet = []
 
-
+#returns row with smallest digit and leftover as tuple
 def row(s):
 
     if len(s) == 0:
@@ -17,6 +17,24 @@ def row(s):
         s.remove(x)
     return r, s
 
+#tries to concatenate leftover to one of the rows
+def concatenateLeftover(t):
+    r = t[-2]
+    l = t[-1]
+
+    ln = []
+    rn = []
+
+    for x in t[-2]:
+        if len(rows(x + l)[-1]) == 0:
+            rn = rn + rows(x + l)[-2]
+            l = []
+        else:
+            rn = rn + [x]
+    return rn, l
+
+
+#returns list of all rows >2 and leftover as tuple
 def rows(s):
     l = s
     r = []
@@ -34,7 +52,8 @@ def rows(s):
 
     return result, l
 
-def placeLeftover(t):
+#splits rows and insert leftover if possible, returns new list of rows and leftover as tuple
+def insertLeftover(t):
     r = t[-2]
     l = t[-1]
     ln = l
@@ -46,6 +65,7 @@ def placeLeftover(t):
         for i in range(3, len(x)-1):
             ln = x[i:] + l
             a = rows(ln)
+
             if len(a[-1]) == 0:
                 r.remove(x)
                 x = x[:i]
@@ -57,20 +77,51 @@ def placeLeftover(t):
                         lnn = lnn + j
                         r.remove(j)
                 return r, lnn
-
     return t
 
+#def insertLeftoverSeperate(s):
 
+def finalRows(l):
+    ##################
+    print("eingabe: ")
+    print(l)
+    ##################
 
+    numberSet = l
+    a = rows(numberSet)
+    result = a
+    ln = []
+
+    for x in a[-1]:
+        result = insertLeftover((result[-2], [x]))
+        ln = ln + result[-1]
+
+    result = result[-2], ln
+
+    ln = []
+
+    if len(result[-1]) > 0:
+        for x in result[-1]:
+            result = concatenateLeftover((result[-2], [x]))
+            ln = ln + result[-1]
+
+    result = result[-2], ln
+
+    #######################
+    print("ergebnis :")
+    print(result[-2])
+    print("uebrig bleibt: ")
+    print(result[-1])
+    ########################
 
 def main():
     #x = input("bitte Zahlen mit Komma getrennt eingeben eingeben ")
     #numberSet = list(x)
-    numberSet = [1,2,3,4,5,6,7,8,9,10,7,4]
-    a = rows(numberSet)
-    temp = a
-    for x in a[-1]:
-        temp = placeLeftover((temp[-2], [x]))
 
-    print(temp)
+    finalRows([1,2,3,4,5,3])
+    finalRows([1,2,3,2,3,4,3,4,5,4,5,6,5,6,7,6,7,8,7,8,9,10])
+    finalRows([1,2,3,4,5,6,7,8,5,6,3,4])
+    finalRows([1,2,3,4,5,4,3])
+    finalRows([1,2,3,4,5,4,3,4])
+
 main()
